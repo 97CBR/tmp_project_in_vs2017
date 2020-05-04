2df986f2f95db1e53948dfdaf031d7a751b22529
MARXCBR <824368602@qq.com>
2020-05-04 20:48:32 +0800

    添加对象池类

---
+++ tmp_project/cbr_object_pool/CbrObjectPool.cpp
@@ -0,0 +1,11 @@
+﻿#include "CbrObjectPool.h"
+
+
+
+CbrObjectPool::CbrObjectPool() {
+    cout << "创建线程池" << endl;
+}
+
+CbrObjectPool::~CbrObjectPool() {
+    cout << "析构对象池" << endl;
+}
---
+++ tmp_project/cbr_object_pool/CbrObjectPool.h
@@ -0,0 +1,11 @@
+﻿#pragma once
+#include <iostream>
+
+using namespace std;
+
+class CbrObjectPool {
+    public:
+  CbrObjectPool();
+  ~CbrObjectPool();
+
+};
